af5a7c810033fcc3251ba7e60fd1864a52ba4916
Robert Heller <heller@deepsoft.com>
2023-05-02 13:24:51 -0400

    Add plywood and foam sheets.

--- Benchwork.py
+++ Benchwork.py
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Tue May 2 11:10:49 2023
-#  Last Modified : <230502.1230>
+#  Last Modified : <230502.1322>
 #
 #  Description	
 #
@@ -103,6 +103,46 @@ class L4x4Leg(object):
         obj.Label=self.name
         obj.ViewObject.ShapeColor=tuple([210/255,180/255,140/255])
     
+class Plywood(object):
+    def __init__(self,name,origin,thickness=3/8,width=48,length=96,cutlist=[]):
+        self.name = name
+        if not isinstance(origin,Base.Vector):
+            raise RuntimeError("origin is not a Vector!")
+        self.origin = origin
+        self.thickness = thickness
+        self.width = width
+        self.length = length
+        self._body =  Part.makePlane(self.length,self.width,self.origin)\
+                        .extrude(Base.Vector(0,0,self.thickness))
+        for c in cutlist:
+            self._body = self._body.cut(c)
+    def show(self):
+        doc = App.activeDocument()
+        obj = doc.addObject("Part::Feature",self.name)
+        obj.Shape=self._body
+        obj.Label=self.name
+        obj.ViewObject.ShapeColor=tuple([210/255,180/255,140/255])
+        
+
+class PinkFoam(object):
+    def __init__(self,name,origin,thickness=2,width=48,length=96,cutlist=[]):
+        self.name = name
+        if not isinstance(origin,Base.Vector):
+            raise RuntimeError("origin is not a Vector!")
+        self.origin = origin
+        self.thickness = thickness
+        self.width = width
+        self.length = length
+        self._body =  Part.makePlane(self.length,self.width,self.origin)\
+                        .extrude(Base.Vector(0,0,self.thickness))
+        for c in cutlist:
+            self._body = self._body.cut(c)
+    def show(self):
+        doc = App.activeDocument()
+        obj = doc.addObject("Part::Feature",self.name)
+        obj.Shape=self._body
+        obj.Label=self.name
+        obj.ViewObject.ShapeColor=tuple([255/255,182/255,193/255])
 
 class WendellDepot_Benchwork(object):
     _totalWidth=14*12
@@ -162,34 +202,62 @@ class WendellDepot_Benchwork(object):
                                         L2x4.Width(),\
                                         self._offFloor-L2x4.Height()))
         self._h8 = L2x4("h8",h8O,self._totalHeight-L2x4.Width()*2,'vertical')
-        self._leg1 = L4x4Leg("l1",self.origin,self._offFloor,\
+        self._leg1 = L4x4Leg("leg1",self.origin,self._offFloor,\
                              [self._l1._body,self._h1._body])
         
         leg2O = self.origin.add(Base.Vector(0,\
                                             self._totalHeight-L4x4Leg.Width(),\
                                             0))
-        self._leg2 = L4x4Leg("l2",leg2O,self._offFloor,\
+        self._leg2 = L4x4Leg("leg2",leg2O,self._offFloor,\
                              [self._l2._body,self._h1._body])
         leg3O = self.origin.add(Base.Vector((self._totalWidth/2)-(L4x4Leg.Width()/2),
                                             0,0))
-        self._leg3 = L4x4Leg("l3",leg3O,self._offFloor,\
+        self._leg3 = L4x4Leg("leg3",leg3O,self._offFloor,\
                              [self._l1._body,self._h4._body])
         leg4O = self.origin.add(Base.Vector((self._totalWidth/2)-(L4x4Leg.Width()/2),
                                             self._totalHeight-L4x4Leg.Width(),\
                                             0))
-        self._leg4 = L4x4Leg("l4",leg4O,self._offFloor,\
+        self._leg4 = L4x4Leg("leg4",leg4O,self._offFloor,\
                              [self._l2._body,self._h4._body])
 
         leg5O = self.origin.add(Base.Vector(self._totalWidth-L4x4Leg.Width(),
                                             0,0))
-        self._leg5 = L4x4Leg("l5",leg5O,self._offFloor,\
+        self._leg5 = L4x4Leg("leg5",leg5O,self._offFloor,\
                              [self._l1._body,self._h8._body])
         
         leg6O = self.origin.add(Base.Vector(self._totalWidth-L4x4Leg.Width(),\
                                             self._totalHeight-L4x4Leg.Width(),\
                                             0))
-        self._leg6 = L4x4Leg("l6",leg6O,self._offFloor,\
+        self._leg6 = L4x4Leg("leg6",leg6O,self._offFloor,\
                              [self._l2._body,self._h8._body])
+        sheet1O = self.origin.add(Base.Vector(0,0,self._offFloor))
+        self._sheet1 = Plywood("sheet1",sheet1O)
+        sheet2O = sheet1O.add(Base.Vector(self._sheet1.length,0,0))
+        self._sheet2 = Plywood("sheet2",sheet2O,3/8,48,\
+                                self._totalWidth-self._sheet1.length)
+        sheet3O = self.origin.add(Base.Vector(0,self._sheet1.width,\
+                                                self._offFloor))
+        self._sheet3 = Plywood("sheet3",sheet3O,3/8,\
+                                self._totalHeight-self._sheet1.width)
+        sheet4O = sheet1O.add(Base.Vector(self._sheet1.length,self._sheet2.width,0))
+        self._sheet4 = Plywood("sheet4",sheet4O,3/8,\
+                                self._totalHeight-self._sheet2.width,\
+                                self._totalWidth-self._sheet1.length)
+
+        foam1O = self.origin.add(Base.Vector(0,0,\
+                                self._offFloor+self._sheet1.thickness))
+        self._foam1 = PinkFoam("foam1",foam1O)
+        foam2O = foam1O.add(Base.Vector(self._foam1.length,0,0))
+        self._foam2 = PinkFoam("foam2",foam2O,2,48,\
+                                self._totalWidth-self._foam1.length)
+        foam3O = self.origin.add(Base.Vector(0,self._foam1.width,
+                                self._offFloor+self._sheet3.thickness))
+        self._foam3 = PinkFoam("foam3",foam3O,2,\
+                                self._totalHeight-self._foam1.width)
+        foam4O = foam1O.add(Base.Vector(self._foam1.length,self._foam2.width,0))
+        self._foam4 = PinkFoam("foam4",foam4O,2,\
+                                self._totalHeight-self._foam2.width,\
+                                self._totalWidth-self._foam1.length)
         
     def show(self):
         self._l1.show()
@@ -208,6 +276,14 @@ class WendellDepot_Benchwork(object):
         self._leg4.show()
         self._leg5.show()
         self._leg6.show()
+        self._sheet1.show()
+        self._sheet2.show()
+        self._sheet3.show()
+        self._sheet4.show()
+        self._foam1.show()
+        self._foam2.show()
+        self._foam3.show()
+        self._foam4.show()
 
 if __name__ == '__main__':
     App.ActiveDocument=App.newDocument("Temp")
