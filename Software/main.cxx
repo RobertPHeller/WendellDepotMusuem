b43d5ce0a5637918a4c3cfc1acb647037ff3ffee
Robert Heller <heller@deepsoft.com>
2024-12-11 09:35:41 -0500

    Basic framework for software.

---
+++ Software/main.cxx
@@ -0,0 +1,65 @@
+// -!- C++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Wed Dec 11 09:18:34 2024
+//  Last Modified : <241211.0930>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+/// @copyright
+///    Copyright (C) 2024  Robert Heller D/B/A Deepwoods Software
+///			51 Locke Hill Road
+///			Wendell, MA 01379-9728
+///
+///    This program is free software; you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation; either version 2 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program; if not, write to the Free Software
+///    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/// @file main.cxx
+/// @author Robert Heller
+/// @date Wed Dec 11 09:18:34 2024
+/// 
+///
+//////////////////////////////////////////////////////////////////////////////
+
+static const char rcsid[] = "@(#) : $Id$";
+
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "os/os.h"
+#include "nmranet_config.h"
+
+#include "openlcb/SimpleStack.hxx"
+#include "openlcb/ConfiguredConsumer.hxx"
+#include "openlcb/ConfiguredProducer.hxx"
+
+
+int appl_main(int argc, char *argv[])
+{
+    return 0;
+}
+
