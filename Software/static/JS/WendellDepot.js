99df2c2118ee6c5277276f6b84653466d6d307ad
Robert Heller <heller@deepsoft.com>
2025-02-27 09:59:54 -0500

    Add static webcontent.

---
+++ Software/static/JS/WendellDepot.js
@@ -0,0 +1,19 @@
+/* -*- javascript -*- 
+     Copyright 2025 Deepwoods Software
+     All Rights Reserved
+     System        : WENDELLDEPOT_JS : 
+     Object Name   : $RCS_FILE$
+     Revision      : $REVISION$
+     Date          : Thu Feb 27 09:52:18 2025
+     Created By    : Robert Heller, Deepwoods Software
+     Created       : Thu Feb 27 09:52:18 2025
+
+     Last Modified : <250227.0952>
+     ID            : $Id$
+     Source        : $Source$
+     Description	
+     Notes
+*/
+var ns4 = document.layers;
+var ie4 = document.all;
+var nn6 = document.getElementById && !document.all;
