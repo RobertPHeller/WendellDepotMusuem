1da518aaa72b98f0400ac11d58c63ee116630916
Robert Heller <heller@deepsoft.com>
2025-04-26 18:15:48 -0400

    Add comments to script.

--- Software/JMRI-CTC-Occupancy.py
+++ Software/JMRI-CTC-Occupancy.py
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Sat Apr 26 10:20:35 2025
-#  Last Modified : <250426.1627>
+#  Last Modified : <250426.1815>
 #
 #  Description	
 #
@@ -44,7 +44,13 @@
 import jarray
 import jmri
 
+#** Class that uses optical sensors to implement block occupancy.
+
 class opticalSensorToBlockOccupancy(jmri.jmrit.automat.AbstractAutomaton):
+    #** Sensor to Signal Plate direction map.  This dictionary maps optical
+    #* sensors to the a Signal Plate direction lamp dictionary, which in
+    #* turn maps to optical sensor(s) at the end of the block and to the block
+    #* that becomes occupied / unoccupied.
     sensorSigPlateMap = { \
             "West Stage Exit 1" : {"Signal Lamp Stage West 1-3L": ("East Stage Exit 1", "Stage_1"), \
                                    "Signal Lamp Stage West 1-3R": ("Stage West 1-3 Entrance", "OS Stage_West_1_3")}, \
@@ -83,6 +89,8 @@ class opticalSensorToBlockOccupancy(jmri.jmrit.automat.AbstractAutomaton):
             "East Stage Exit 4" : {"Signal Lamp Stage East 2-4L": ("Stage East 2-4 Entrance", "OS Stage_East_2_4"),\
                                    "Signal Lamp Stage East 2-4R": ("West Stage Exit 4", "Stage_4")} \
     }
+    #** This dictionary maps signals that need to fall down (change to Stop)
+    #* when a block is entered.
     blockProtectionSignals = { \
         "OS Stage_West_1_3": ["WestYardEntry1-3", "WestExit1 (Dwarf)", "WestExit3 (Dwarf)"], \
         "OS Stage_East_1_3": ["EastYardEntry1-3", "EastExit1 (Dwarf)", "EastExit3 (Dwarf)"], \
@@ -91,69 +99,76 @@ class opticalSensorToBlockOccupancy(jmri.jmrit.automat.AbstractAutomaton):
         "OS West_Double_Track": ["WestEN", "WestF1", "WestFM"], \
         "OS East_Double_Track": ["EastEN", "EastF1", "EastFM"] \
     }
+    #** Initialize things: build a list of NameBeans (optical sensors) to wait
+    #* on, stash their initial state, and create an empty dictionary for 
+    #* run time state information.
     def init(self):
         self.opticalSensors = list()
         self.sensorKnownStates = dict()
         self.sensorStates = dict()
         for key in self.sensorSigPlateMap:
-            print key
+            #print key
             sensor = sensors.getSensor(key)
-            print sensor
+            #print sensor
             self.opticalSensors.append(sensor)
             self.sensorKnownStates[key] = sensor.getKnownState()
+    #** Running handler: wait for a sensor to trip, then process the event.
     def handle(self):
+        #* Wait...
         self.waitChange(self.opticalSensors)
+        #* find the changed sensor(s)
         for s in self.opticalSensors:
+            #* Get name of the sensor
             name = s.getUserName()
-            print name,":",s
-            print self.sensorKnownStates[name], s.getKnownState()
+            #print name,":",s
+            #print self.sensorKnownStates[name], s.getKnownState()
+            #* Has its state changed?
             if self.sensorKnownStates[name] != s.getKnownState():
-                print name, s.getKnownState()
+                #print name, s.getKnownState()
+                #* update stored state
                 self.sensorKnownStates[name] = s.getKnownState()
+                #* Sensor covered?
                 if s.getKnownState() == ACTIVE:
+                    #* yes, loop through the sub-dictionary
                     for k in self.sensorSigPlateMap[name]:
+                        #* Found the direction lamp?
                         if sensors.getSensor(k).getKnownState() == ACTIVE:
-                            otherends, blocklist = self.sensorSigPlateMap[name][k]
-                            if type(blocklist) is list:
-                                for b in blocklist:
-                                    block = blocks.getBlock(b)
-                                    print "Entering:",b,block
-                                    block.getSensor().setKnownState(ACTIVE)
-                                    if self.blockProtectionSignals.has_key(b):
-                                        for sig in self.blockProtectionSignals[b]:
-                                            mast = masts.getSignalMast(sig)
-                                            mast.setAspect("Stop")
-                            else:
-                                block = blocks.getBlock(blocklist)
-                                print "Entering:",blocklist,block
-                                block.getSensor().setKnownState(ACTIVE)
-                                if self.blockProtectionSignals.has_key(blocklist):
-                                    for sig in self.blockProtectionSignals[blocklist]:
-                                        mast = masts.getSignalMast(sig)
-                                        mast.setAspect("Stop")
+                            #* Yes, grab the far end sensors and the affeted block
+                            otherends, blockname = self.sensorSigPlateMap[name][k]
+                            #* get the actual block
+                            block = blocks.getBlock(blockname)
+                            #print "Entering:",blockname,block
+                            #* Set the block occupied
+                            block.getSensor().setKnownState(ACTIVE)
+                            #* Are there signals that need to fall?
+                            if self.blockProtectionSignals.has_key(blocklist):
+                                #* Yes, drop them (set their aspects to Stop)
+                                for sig in self.blockProtectionSignals[blocklist]:
+                                    mast = masts.getSignalMast(sig)
+                                    mast.setAspect("Stop")
+                            #* Stash the other ends
                             if type(otherends) is list:
                                 for o in otherends:
-                                    self.sensorStates[o] = blocklist
+                                    self.sensorStates[o] = blockname
                             else:
-                                self.sensorStates[otherends] = blocklist
+                                self.sensorStates[otherends] = blockname
                 else:
+                    #* Sensor cleared, unoccupy the block, if we are waiting
                     if self.sensorStates.has_key(name):
-                        blocklist = self.sensorStates[name]
+                        blockname = self.sensorStates[name]
                         del self.sensorStates[name]
-                        if type(blocklist) is list:
-                            for b in blocklist:
-                                block = blocks.getBlock(b)
-                                print "Exiting:",b,block
-                                block.getSensor().setKnownState(INACTIVE)
-                        else:
-                            block = blocks.getBlock(blocklist)
-                            print "Exiting:",blocklist,block
-                            block.getSensor().setKnownState(INACTIVE)
-                break
+                        block = blocks.getBlock(blockname)
+                        print "Exiting:",blockname,block
+                        block.getSensor().setKnownState(INACTIVE)
+                #break
+        #* Wait for more
         return True
 
+#* Create an instance
 o = opticalSensorToBlockOccupancy();
 
+#* Set its name
 o.setName("Optical Sensor To Block Occupancy")
 
+#* start it running
 o.start()
